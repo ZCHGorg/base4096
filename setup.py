df0a80f0963a01f446e53d69569c3f2941808a3a
Josef Kulovany <stealthmachines@gmail.com>
2025-06-06 01:49:22 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='base4096',
-    version='1.0',
+    version='2.0',
     packages=find_packages(),
     include_package_data=True,
     package_data={
