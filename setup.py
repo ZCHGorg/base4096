36c70a9841d5fd0dffaa9114d9899b87168a24f5
Josef Kulovany <stealthmachines@gmail.com>
2025-07-18 02:30:53 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='https://github.com/ZCHGorg/base4096',
     license='https://zchg.org/t/legal-notice-copyright-applicable-ip-and-licensing-read-me/440',
     keywords='base4096 encoder decoder',
-    install_requires=['unicodedata'],  # Add unicodedata if needed
+    install_requires=[],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: ZCHG.ORG LICENSE - https://zchg.org/t/legal-notice-copyright-applicable-ip-and-licensing-read-me/440',
