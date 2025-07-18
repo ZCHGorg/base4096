4062121431f08e9cb783db0141ea7ed349c3328e
Josef Kulovany <stealthmachines@gmail.com>
2025-07-18 03:20:42 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='base4096',
     version='2.0',
-    packages=['base4096'],  
     py_modules=[
         'base4096',
+        'base4096_shim',
         'frozen_base4096_alphabet',
         'freeze_base4096_alphabet',
         'sign_base4096',
-        'base4096_hkdf_seal'
+        'base4096_hkdf_seal',
     ],
+    packages=['base4096'],  # Only if you include base4096/__init__.py
     include_package_data=True,
-    package_data={
-        '': ['*.py', '*.txt'],
-    },
+    package_data={'': ['*.txt']},
     description='Base4096 encoding and decoding functions',
     author='Josef Kulovany',
     author_email='charg.chg.wecharg@gmail.com',
