1b7e8b23376f5ecca6797a2f53a3295de1cb3f68
Josef Kulovany <stealthmachines@gmail.com>
2025-07-18 03:07:18 -0600

    Update __init__.py

--- base4096/__init__.py
+++ base4096/__init__.py
@@ -1,4 +1,10 @@
-# base4096/__init__.py
+import sys
+import os
+
+# Allow import from repo root even if not in PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from base4096 import encode, decode
 from frozen_base4096_alphabet import BASE4096_ALPHABET, CHAR_TO_INDEX
+
 __version__ = '2.0'
